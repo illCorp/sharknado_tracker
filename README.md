5aa871914734510e95ed12c699e483599614e57b
Mike Orr <mikeorr85@gmail.com>
2014-03-24 18:04:10 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#README
+
+**Sharknado Tracker** is an example Rails app demonstrating usage of the [illlocation Rails Engine](https://github.com/illCorp/illlocation).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharknadoTracker::Application.config.secret_key_base = '048cde03877bc9aa577aba686b2b8bb957994e08a594e0e396d988bfda0a4964c127605e4b6b22033f13001429f586796a847778470750fb06bf2ee8927a90de'
